1eb82e73e7529ccd4f8d49d8041aef06b6c71f2b
Srinivash Dev <82706452+CByadd@users.noreply.github.com>
2023-10-28 20:39:39 +0530

    Update Post6.js

--- components/Post6.js
+++ components/Post6.js
@@ -52,6 +52,48 @@ function Post({id,username,userImg,img,caption}) {
   </form>
 </div>
 
+<div className='relative absolute child mt-7'>
+<div className='flex items-center '>
+    <img src="1.png"alt='' className='rounded-full h-11 w-11 object-cover ml-3 mr-3 ' />
+    <p className='text-white'>ISRO</p>
+    <DotsHorizontalIcon className=' h-5 pl-96 text-white Do'/>
+    </div>
+  <div className='bg-slate-950 mt-3'>
+  <img src='https://akm-img-a-in.tosshub.com/businesstoday/images/story/202307/ezgif-sixteen_nine_300.jpg?size=948:533'
+    className='flex object-cover w-fit'/>
+  </div>
+
+  <div className='flex justify-between px-4 pt-4 text-white'>
+    <div className='flex space-x-4'>
+      <HeartIcon className='btn'/>
+      <ChatIcon className='btn'/>
+      <PaperAirplaneIcon className='btn'/>
+    </div>
+
+    <BookmarkIcon className='btn'/>
+  </div>
+
+<div>
+  <p className='p-5 truncate text-white'>
+    <span className='font-bold mr-4'>ISRO</span>
+        This is MEGA project done by Holy Cross Mat. school With the Students Involves <br> Adithiyan, A R rehman, Kamalesh R, Srinivash R T.
+  </p>
+</div>
+
+<div>
+  <form className='flex items-center p-4'>
+    <EmojiHappyIcon className='h-7 text-white'/>
+    <input type='text'  placeholder='Add a comment' className=' ml-3 mr-3 border-none outline-none flex-1 bg-black text-white'
+    />
+    <button className='text-white'>Post</button>
+  </form>
+</div>
+
+
+</div>
+
+
+            
 
 </div>
 
